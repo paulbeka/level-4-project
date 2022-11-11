7bb88bb0a2b928e167161d3733dc9ad4b9f644e1
Paul Bekaert <2517073B@student.gla.ac.uk>
2022-11-11 17:33:03 +0000

    started work on removing 1 cell from every other spaceship

--- source/dataloaders/spaceship_comparisons_dataloader.py
+++ source/dataloaders/spaceship_comparisons_dataloader.py
@@ -1,6 +1,12 @@
+import sys
+
+sys.path.insert(1, "C:\\Workspace\\level-4-project\\source")
+
+from game_of_life import Game
 from tools.rle_reader import RleReader
 import numpy as np
 import random
+import os
 
 
 class SpaceshipCompareDataloader:
@@ -8,23 +14,75 @@ class SpaceshipCompareDataloader:
 	def __init__(self,
 				n_samples,
 				root_folder,
+				delete_count,
+				exclude_ratio = 0,
 				ratio=0.8):
 		
 		self.n_samples = n_samples
 		self.root_folder = root_folder
+		self.n_delete_cells = delete_count
+		self.exclude_ratio = exclude_ratio
 
 		self.n_train_samples = int(n_samples * ratio)
 
 		self.reader = RleReader()
 
 
-	def loadSpaceships(self):
-		spaceships = self.reader.getFileArray(os.path.join(self.root_folder, "spaceships.txt"))
+	def loadSpaceships(self, spaceships, width, height):
 
-		modified_spaceships = []
+		configurations = []
 
 		for i in range(self.n_samples):
 			spaceship = spaceships[i % len(spaceships)]
 
-			# randomly remove parts of the spaceship
+			newGrid = np.zeros((width, height))
+
+			alive = np.argwhere(spaceship == 1)	
+			# randomly remove parts of the spaceship (as you don't want to favour specific ships)
 			if bool(random.getrandbits(1)):
+				for i in range(min(self.n_delete_cells, len(alive) - 5)):	# make sure you don't delete all the cells
+					alive = np.delete(alive, random.randint(0, len(alive)-1))
+
+			newGrid[alive[:, 0], alive[:, 1]] = 1
+			configurations.append(newGrid)
+
+		return configurations
+
+
+	def generateSpaceships(self, width, height):
+		spaceships = self.reader.getFileArray(os.path.join(self.root_folder, "spaceships.txt"))
+
+		if self.exclude_ratio > 0:
+			excluded_spaceship_indexes = random.sample(range(0, len(spaceships)-1), len(spaceships)*self.exclude_spaceships_ratio)
+			excluded_spaceships = [spaceships[i] for i in excluded_spaceship_indexes]
+			non_excluded_spaceships = [spaceships[i] for i in range(len(spaceships)-1) if i not in excluded_spaceship_indexes]
+
+			excluded_configs = self.loadSpaceships(excluded_spaceships, width, height)[0:self.n_train_samples]
+			non_excluded_configs = self.loadSpaceships(non_excluded_spaceships, width, height)[self.n_train_samples:]
+
+			return excluded_configs + non_excluded_configs
+
+		else:
+			return self.loadSpaceships(spaceships, width, height)
+
+
+	def loadData(self, width, height):
+		spaceship_configs = self.generateSpaceships(width, height)
+
+		train_dataset = spaceship_configs[0:self.n_train_samples]
+		test_dataset = spaceship_configs[self.n_train_samples:]
+
+		train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=self.batch_size, shuffle=True)
+		test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=self.batch_size, shuffle=True)
+
+		game = Game(100, 100)
+		game.renderItemList([item[0] for item in list(test_loader)])
+		game.run()
+		game.kill()
+
+		return train_loader, test_loader
+
+
+if __name__ == '__main__':
+	dl = SpaceshipCompareDataloader(1000, os.path.join("C:\\Workspace\\level-4-project\\source\\data", "spaceship_identification"), 3)
+	dl.loadData(100, 100)
--- source/dataloaders/spaceshipid_dataloader.py
+++ source/dataloaders/spaceshipid_dataloader.py
@@ -39,12 +39,9 @@ class SpaceshipIdentifierDataLoader:
 
 		self.reader = RleReader()
 
-		self.spaceships = []
 
+	def loadSpaceships(self, spaceships, width, height):
 
-	def loadSpaceships(self, width, height):
-		# save the spaceships in case of future use
-		self.spaceships = self.reader.getFileArray(os.path.join(self.root_folder, "spaceships.txt"))
 		randomly_placed_spaceships = []
 
 		generator = RleGenerator(width, height)
@@ -53,7 +50,7 @@ class SpaceshipIdentifierDataLoader:
 		for i in range(self.n_samples):
 			
 			# need to loop around the only spaceships that we have available
-			currSpaceship = self.spaceships[i % len(self.spaceships)]
+			currSpaceship = spaceships[i % len(spaceships)]
 			aliveLoc = np.argwhere(currSpaceship == 1)
 
 			# using the max function to make sure it does not crash if shape[x]-1 = -1
@@ -99,41 +96,65 @@ class SpaceshipIdentifierDataLoader:
 				randomly_placed_spaceships.append(grid)
 
 		game.kill()
+
 		return [(item, 1) for item in randomly_placed_spaceships]
 
 
 	# function used to generate an equal number of ships and random patterns in an n*m grid
 	# width, height : dimentions of the grid
 	def loadData(self, width, height):
-		spaceship_configs = self.loadSpaceships(width, height)
+		spaceships = self.reader.getFileArray(os.path.join(self.root_folder, "spaceships.txt"))
+		spaceship_configs = []
 
-		# use (item, 0|1) to signify spaceship or not
+		# generates the spaceship configurations
+		if self.exclude_spaceships_ratio > 0:
+			# seperate into 2 different spaceship lists (excluded spaceships are those put in validation dataset)
+			excluded_spaceship_indexes = random.sample(range(0, len(spaceships)-1), len(spaceships)*self.exclude_spaceships_ratio)
+			excluded_spaceships = [spaceships[i] for i in excluded_spaceship_indexes]
+			non_excluded_spaceships = [spaceships[i] for i in range(len(spaceships)) if i not in excluded_spaceship_indexes]
+
+			spaceship_configs += self.loadSpaceships(non_excluded_spaceships, width, height)[0:self.n_train_samples]
+			spaceship_configs += self.loadSpaceships(excluded_spaceships, width, height)[self.n_train_samples:]
+
+		else:
+			spaceship_configs = self.loadSpaceships(spaceships, width, height)
+
+		#load the random configs
 		if self.fixed_box_size:
 			generator = RleGenerator(width, height)
-			box_sizes = self.getBoxSizes()
-			random_configs = [(item, 0) for item in generator.generateRandomInsideBoxSize(self.n_samples, self.random_density, box_sizes)]
+			if self.exclude_spaceships_ratio > 0:
+				non_excluded_box_sizes = self.getBoxSizes(non_excluded_spaceships)
+				random_configs = [(item, 0) for item in generator.generateRandomInsideBoxSize(self.n_samples, self.random_density, non_excluded_box_sizes)][0:self.n_train_samples]
+				excluded_box_sizes = self.getBoxSizes(excluded_spaceships)
+				random_configs += [(item, 0) for item in generator.generateRandomInsideBoxSize(self.n_samples, self.random_density, excluded_box_sizes)][self.n_train_samples:]
+
+			else:
+				box_sizes = self.getBoxSizes(spaceships)
+				random_configs = [(item, 0) for item in generator.generateRandomInsideBoxSize(self.n_samples, self.random_density, box_sizes)]
+		# load random data onto the board FIX THIS ITS BROKEN CAUSE THE FILE HAS ONLY 100x100 SIZE STRUCTURES
 		else:
 			random_configs = [(item, 0) for item in self.reader.getFileArray(os.path.join(self.root_folder,"random_rles.txt"))]
 
+		# create the datset and put inside of a dataloader class
 		train_dataset = random_configs[0:self.n_train_samples] + spaceship_configs[0:self.n_train_samples]
 		test_dataset = random_configs[self.n_train_samples:] + spaceship_configs[self.n_train_samples:]
 
 		train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=self.batch_size, shuffle=True)
 		test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=self.batch_size, shuffle=True)
 
-		# # TESTING ONLY
-		# game = Game(100, 100)
-		# game.renderItemList([item[0] for item in list(test_loader)])
-		# game.run()
+		# TESTING ONLY
+		game = Game(100, 100)
+		game.renderItemList([item[0] for item in list(test_loader)])
+		game.run()
 		game.kill()
 
 		return train_loader, test_loader
 
 
 	# get the box sizes of spaceships
-	def getBoxSizes(self):
+	def getBoxSizes(self, spaceships):
 		box_sizes = []
-		for item in self.spaceships:
+		for item in spaceships:
 			alive = np.argwhere(item == 1)
 			width = max(alive[:,0]) - min(alive[:, 0]) + 1
 			height = max(alive[:,1]) - min(alive[:,1]) + 1
